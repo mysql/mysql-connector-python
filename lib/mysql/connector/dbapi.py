6ec1e7467cd4d7316625b43994f4ca823cc30b9e
Geert Vanderkelen <geert.vanderkelen@oracle.com>
2014-06-26 13:45:04 +0200

    Prepare release 2.0.0a1

--- lib/mysql/connector/dbapi.py
+++ lib/mysql/connector/dbapi.py
@@ -1,5 +1,5 @@
 # MySQL Connector/Python - MySQL driver written in Python.
-# Copyright (c) 2009, 2013, Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 2009, 2014, Oracle and/or its affiliates. All rights reserved.
 
 # MySQL Connector/Python is licensed under the terms of the GPLv2
 # <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most
