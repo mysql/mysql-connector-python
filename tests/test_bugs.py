4e42684eda3cb7006389583f924ef4c2a8c84ab1
Peeyush Gupta <peeyush.x.gupta@oracle.com>
2015-05-07 14:11:07 +0530

    BUG20834643: Attribute Error while promoting servers using MySQL Fabric
    An AttributeError was occuring while using Connector/Python with
    MySQL Fabric, the reason being named tuple cursor returning wrong
    results with fetchall() method. We fix this by correctly tranforming
    rows to named tuples when fetched with MySQLCursor.fetchall()
    method.
    
    This patch also fixes BUG#20836026.
    
    A test case has been added for BUG#20834643.

--- tests/test_bugs.py
+++ tests/test_bugs.py
@@ -3504,3 +3504,77 @@ class BugOra20811802(tests.MySQLConnectorTests):
         for row in cur:
             self.assertEqual(row, dict(zip(('id', 'name', 'dept'), data[i])))
             i += 1
+
+
+class BugOra20834643(tests.MySQLConnectorTests):
+    """BUG#20834643: ATTRIBUTE ERROR NOTICED WHILE TRYING TO PROMOTE SERVERS
+    """
+    def setUp(self):
+        config = tests.get_mysql_config()
+        cnx = connection.MySQLConnection(**config)
+        cur = cnx.cursor()
+
+        self.tbl = 'Bug20834643'
+        cur.execute("DROP TABLE IF EXISTS {0}".format(self.tbl))
+
+        create = ("CREATE TABLE {0} (id INT, name VARCHAR(5), dept VARCHAR(5)) "
+                  "DEFAULT CHARSET latin1".format(self.tbl))
+        cur.execute(create)
+        cur.close()
+        cnx.close()
+
+    def tearDown(self):
+        config = tests.get_mysql_config()
+        cnx = connection.MySQLConnection(**config)
+        cur = cnx.cursor()
+        cur.execute("DROP TABLE IF EXISTS {0}".format(self.tbl))
+        cur.close()
+        cnx.close()
+
+    @foreach_cnx()
+    def test_set(self):
+        cur = self.cnx.cursor()
+        sql = "INSERT INTO {0} VALUES(%s, %s, %s)".format(self.tbl)
+
+        data = [
+            (1, 'abc', 'cs'),
+            (2, 'def', 'is'),
+            (3, 'ghi', 'cs'),
+            (4, 'jkl', 'it'),
+        ]
+        cur.executemany(sql, data)
+        cur.close()
+
+        cur = self.cnx.cursor(named_tuple=True)
+        cur.execute("SELECT * FROM {0}".format(self.tbl))
+
+        res = cur.fetchone()
+        self.assertEqual(data[0], (res.id, res.name, res.dept))
+        res = cur.fetchall()
+        exp = []
+        for row in res:
+            exp.append((row.id, row.name, row.dept))
+        self.assertEqual(exp, data[1:])
+        cur.close()
+
+        cur = self.cnx.cursor(named_tuple=True, buffered=True)
+        cur.execute("SELECT * FROM {0}".format(self.tbl))
+        res = cur.fetchone()
+        self.assertEqual(data[0], (res.id, res.name, res.dept))
+        res = cur.fetchall()
+        exp = []
+        for row in res:
+            exp.append((row.id, row.name, row.dept))
+        self.assertEqual(exp, data[1:])
+        cur.close()
+
+        cur = self.cnx.cursor(named_tuple=True, buffered=False)
+        cur.execute("SELECT * FROM {0}".format(self.tbl))
+        res = cur.fetchone()
+        self.assertEqual(data[0], (res.id, res.name, res.dept))
+        res = cur.fetchall()
+        exp = []
+        for row in res:
+            exp.append((row.id, row.name, row.dept))
+        self.assertEqual(exp, data[1:])
+        cur.close()
