2fd0bc2136084713edb719f7b8cfec72696bf0eb
Nuno Mariz <nuno.mariz@oracle.com>
2016-05-25 21:51:12 +0100

    Add copyright information in protobuf files

--- lib/mysqlx/protobuf/t.py
+++ lib/mysqlx/protobuf/t.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 # MySQL Connector/Python - MySQL driver written in Python.
 # Copyright (c) 2016, Oracle and/or its affiliates. All rights reserved.
 
@@ -24,10 +23,8 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 import mysqlx_resultset_pb2
-import sys
 
 row = mysqlx_resultset_pb2.Row()
 for x in xrange(0, 100000):
-    row.ParseFromString("\x0a\x0e\x7b\x22\x66\x6f\x6f\x22\x3a\x22\x62\x61\x72\x22\x7d\x00\x0a\x01\x02")
-
-
+    row.ParseFromString("\x0a\x0e\x7b\x22\x66\x6f\x6f\x22\x3a\x22\x62\x61\x72"
+                        "\x22\x7d\x00\x0a\x01\x02")
