73c9c43966f3d81269a836ebc237a6a1ae103b9d
Nuno Mariz <nuno.mariz@oracle.com>
2016-06-03 21:41:27 +0100

    Minor code improvements

--- lib/mysqlx/crud.py
+++ lib/mysqlx/crud.py
@@ -21,6 +21,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
+"""Implementation of the CRUD database objects."""
+
 from .statement import (FindStatement, AddStatement, RemoveStatement,
                         ModifyStatement, SelectStatement, InsertStatement,
                         DeleteStatement, UpdateStatement,
--- lib/mysqlx/dbdoc.py
+++ lib/mysqlx/dbdoc.py
@@ -21,6 +21,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
+"""Implementation of the DbDoc."""
+
 import json
 import uuid
 
