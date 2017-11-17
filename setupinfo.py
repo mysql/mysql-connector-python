fac9f9ecc66c5d45d2acb49332e495192dcfb329
Nuno Mariz <nuno.mariz@oracle.com>
2017-11-17 12:37:20 +0000

    WL11079: Add support for Python 3.6

--- setupinfo.py
+++ setupinfo.py
@@ -129,6 +129,7 @@ classifiers = [
     'Programming Language :: Python :: 3.3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     'Topic :: Database',
     'Topic :: Software Development',
     'Topic :: Software Development :: Libraries :: Application Frameworks',
