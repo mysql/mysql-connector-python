15f9f0c277b3f8ca46ef0399cd69f890e904d7f9
Nuno Mariz <nuno.mariz@oracle.com>
2020-11-17 18:01:46 +0000

    WL14240: Remove Python 3.5 support
    This worklog removes the support for Python 3.5 in MySQL Connector/Python.
    Metadata and packaging scripts were changed accordingly.

--- setup.py
+++ setup.py
@@ -127,7 +127,6 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
