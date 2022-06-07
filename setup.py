07e3d17dc4dcea16f79a8e8364328926f0afc9ff
Nuno Mariz <nuno.mariz@oracle.com>
2022-06-07 10:38:41 +0100

    BUG#34231226: Generated classes do not work with the latest Protobuf
    The latest Python Protobuf package 4.21.0 introduced a breaking change
    in the compatibility between the resulting generated classes from
    protoc and the runtime library.
    
    To keep backward compatibility, Python Protobuf is now required to be
    >=3.11.0 and <=3.20.1. The setup.py `install_requires` option and
    documentation got updated accordingly (added by Git revision 4fb79de).
    
    Optional dependencies updated to the approved versions.
    
    Change-Id: I0ff9dae7c5fc59dece8b7919c04d3163373e96d3

--- setup.py
+++ setup.py
@@ -140,8 +140,8 @@ setup(
     cmdclass=COMMAND_CLASSES,
     install_requires=["protobuf>=3.11.0,<=3.20.1"],
     extras_require={
-        "dns-srv": ["dnspython>=1.16.0"],
-        "compression": ["lz4>=2.1.6", "zstandard>=0.12.0"],
-        "gssapi": ["gssapi>=1.6.9"],
+        "dns-srv": ["dnspython>=1.16.0,<=2.1.0"],
+        "compression": ["lz4>=2.1.6,<=3.1.3", "zstandard>=0.12.0,<=0.15.2"],
+        "gssapi": ["gssapi==1.6.9"],
     },
 )
