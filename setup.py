61dc308fafa586cea9db98131274e9489466bfa9
Klaas van Schelven <klaas@vanschelven.com>
2022-11-09 10:26:55 +0100

    Upgrade max protobuf version
    https://nvd.nist.gov/vuln/detail/CVE-2022-1941

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ setup(
     ],
     ext_modules=EXTENSIONS,
     cmdclass=COMMAND_CLASSES,
-    install_requires=["protobuf>=3.11.0,<=3.20.1"],
+    install_requires=["protobuf>=3.11.0,<=3.20.2"],
     extras_require={
         "dns-srv": ["dnspython>=1.16.0,<=2.1.0"],
         "compression": ["lz4>=2.1.6,<=3.1.3", "zstandard>=0.12.0,<=0.15.2"],
